c2c27c47fb2e8f001bf0ac11c9b5e87ec3a449ce
TeslyukVlad <Tesluakevlad@gmail.com>
2021-01-28 00:09:02 +0200

    +dist

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,29 @@
 jQuery(document).ready(function($) {
 
+	$('#header').scroll();
+
 	$(".scroll_down").on("click", "a", function (event) {
+	    event.preventDefault();
+
+	    var id  = $(this).attr('href'),
+	        top = $(id).offset().top;
+	        
+	    $('body, html').animate({scrollTop: top}, 1100);
+    });
+
+    $("nav #content .nav__navbar .navbar__elems ul li").on("click", "a", function (event) {
+	    event.preventDefault();
+
+	    $('nav #content .nav__navbar .navbar__elems ul li a.active').removeClass('active');
+	    $(this).addClass('active')
+
+	    var id  = $(this).attr('href'),
+	        top = $(id).offset().top;
+	        
+	    $('body, html').animate({scrollTop: top}, 1100);
+    });
+
+    $("article .contact_info .addr .Reservation").on("click", "a", function (event) {
 	    event.preventDefault();
 
 	    var id  = $(this).attr('href'),
@@ -37,6 +60,19 @@ jQuery(document).ready(function($) {
 		$('body').toggleClass('lock');
 	});
 
+	$('nav #content .nav__navbar .navbar__elems ul li a').click(function(event) {
+		if ( $('.nav__navbar').hasClass('active')   ||
+			 $('burger__navbar').hasClass('active') ||
+			 $('.nav__brand').hasClass('active')    ||
+			 $('body').hasClass('lock') ) {
+			
+			$('.nav__navbar').toggleClass('active');
+			$('.burger__navbar').toggleClass('active');
+			$('.nav__brand').toggleClass('active');
+			$('body').toggleClass('lock');
+		}
+	});
+
 	// $(window).on('scroll', function(event) {
 	// 	var wt = $(window).scrollTop();
 	// 	var wh = $(window).height();
@@ -54,5 +90,29 @@ jQuery(document).ready(function($) {
 	// 	}
 	// });
 	
+	$(window).on('scroll', function(event) {
+		var wt = $(window).scrollTop();
+		var wh = $(window).height();
+		var elem = $('.point_blocks');  // тут пишем селектор для выбора коллекции элементов, позиции которых нужно отследить
+		  
+		var et = null, eh = null;
+		 	et = elem.offset().top;  // тут уже получаем параметр от элемента
+		   	eh = elem.outerHeight(); // тут уже получаем параметр от элемента
+				 
+		if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)){
+		    var navbar_elem = $(this, ' a.index').attr('id'),
+		    	nabar_but = $('nav #content .nav__navbar .navbar__elems ul li', navbar_elem);
+
+		    $('nav #content .nav__navbar .navbar__elems ul li a.active').removeClass('active');
+
+		   	nabar_but.addClass('active');
+
+
+		   	console.log('В областе видимости');
+
+		} else {
+		    console.log('Блок скрыт');
+		}
+	});
 });
 
